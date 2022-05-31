5cd384f26986f5adf5e64e9debf740ba3c44b4c2
TingCythia <91254839+TingCythia@users.noreply.github.com>
2022-05-31 15:16:43 +0200

    Navbar to Hanna update

--- checkout-react/src/App.tsx
+++ checkout-react/src/App.tsx
@@ -1,23 +1,6 @@
-import React from 'react';
+import Home from "./pages/home";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+const App=()=>{
+    return<Home/>;
+};
 export default App;
